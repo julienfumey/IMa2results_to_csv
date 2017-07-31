4f0e8390725e892b04581e009ee6865a14ac02f7
Julien Fumey <julien.fumey@gmail.com>
2017-07-31 17:41:08 +0200

    Functional script

--- IMa2results_to_csv.pl
+++ IMa2results_to_csv.pl
@@ -8,11 +8,15 @@ my $infile = IO::File->new("$ARGV[0]", "<");
 
 my $write = 0;
 my $outfile;
+my $histogroup = 0;
+my $countl = 0;
+
 READFILE:while(my $line = <$infile>){
-    my $histogroup = 0;
-    if($line =~ s/^HISTOGRAM GROUP ([0-4])/$1/){
-        $write = 1;
+    if($line =~ /^HISTOGRAM GROUP [0-4]/){
+        $line =~ s/^HISTOGRAM GROUP ([0-9])/$1/;
         $histogroup = $1;
+        print $histogroup;
+        $write = 1;
         next READFILE;
     }
 
@@ -20,31 +24,29 @@ READFILE:while(my $line = <$infile>){
         next READFILE;
     }
 
-    if($line =~ /^Summaries/){
-        $outfile = IO::File->next("$ARGV[0]\_group_$histogroup\_summaries.csv", ">");
+    if($line =~ /^ Summaries/ && $write == 1){
+        print "opening $ARGV[0]\_group_$histogroup.csv\n";
+        $outfile = IO::File->new("$ARGV[0]\_group_$histogroup.csv", ">") or die "Can't open file $!";
         $write = 2;
+        $countl = 1018;
         next READFILE;
     }
 
-    if($line =~ /^Parameter/){
-        $outfile = IO::File->next("$ARGV[0]\_group_$histogroup\_parameter.csv", ">");
-        $write = 2;
-        next READFILE;
-    }
-
-    if ($line =~ /^$/){
+    if ($countl == 0 && $write == 2){
+        print $write."\n";
         $outfile->close();
         $write = 1;
         next READFILE;
     }
 
-    $outfile->write($line);
-
-
     if($write == 1){
         next READFILE;
     }
 
+    if($write == 2 && $countl > 0){
+        $outfile->write($line);
+        $countl -= 1;
+    }
 
     if($line =~ /^ASCII Curves/){
         $write = 0;
